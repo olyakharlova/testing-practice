19e8aa3429fdd091f27a7542e6a548c237cd303a
olya kharlova <pepedrizzle@gmail.com>
2023-02-07 13:18:38 +0300

    capitalize.test.js modified

--- _tests_/capitalize.test.js
+++ _tests_/capitalize.test.js
@@ -1,11 +1,7 @@
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error("Function doesn't work correctly");
-}
-
-if (capitalize('') !== '') {
-  throw new Error("Function doesn't work correctly");
-}
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
 
 console.log('All tests passed');
